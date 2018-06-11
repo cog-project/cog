84482f27c1c6f764376cdeee4880526da5d709a5
Michael LeSane <michael@lasc.home>
2018-06-11 01:14:07 -0500

    main.php add mongo client class

--- main.php
+++ main.php
@@ -212,6 +212,12 @@ class network {
 
 	private $size = 0;
 	private $lastHash = null;
+	private $dbClient;
+
+	public function __construct() {
+		// We should be able to configure this later.
+		$this->dbClient = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+	}
 
 	public function register($party) {
 		$this->publicKeys[$party->getAddress()] = $party->getPublicKey();
@@ -219,8 +225,6 @@ class network {
 
 	public function put($contract) {
 		# nonce - should be at least three, and over threshold of signers
-		print_r($contract);
-		die();
 
 		$contract->finalize($this->size,$this->lastHash);
 		$this->contracts[$contract->getHash()] = array(
@@ -306,6 +310,81 @@ class network {
 	}
 
 	public function mine() {
+	}
+}
+
+class dbClient {
+	protected $client = null;
+
+	public function __construct() {
+		$this->client = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+	}
+
+	public function queryByKey($table,$key = []) {
+		return $this->dbQuery($table,$key)->toArray();
+	}
+
+	public function command($cmd) {
+		return $this->dbCommand("admin",$cmd);
+	}
+
+	public function dbCommand($db,$cmd) {
+		return $this->client->executeCommand($db,new \MongoDB\Driver\Command($cmd));
+	}
+
+	public function dbQuery($db,$key) {
+		return $this->client->executeQuery($db,new \MongoDB\Driver\Query($key));
+	}
+
+	public function dbDelete($table,$key) {
+		$this->dbDeleteMultiple($table,[$key]);
+	}
+	public function dbDeleteMultiple($table,$key) {
+		$bulk = new MongoDB\Driver\BulkWrite;
+		foreach($key as $v) {
+			$bulk->delete($v);
+		}
+		$this->client->executeBulkWrite($table,$bulk);
+	}
+	public function dbInsert($db,$key) {
+		$this->dbInsertMultiple($db,[$key]);
+	}
+	public function dbInsertMultiple($db,$key = []) {
+		$bulk = new MongoDB\Driver\BulkWrite;
+		foreach($key as $v) {
+			$bulk->insert($v);
+		}
+		$this->client->executeBulkWrite($db,$bulk);
+	}
+	
+	public function showDatabases() {
+		$res = $this->dbCommand("admin",["listDatabases"=>1])->toArray();
+		return array_shift($res)->databases;
+	}
+	public function showCollections($db) {
+		$res = $this->dbCommand($db,["listCollections"=>1])->toArray();
+		$out = array_map(function($x) { return $x->name; },$res);
+		return $out;
+	}
+
+	public function getCount($db,$table,$query = []) {
+		$res = $this->dbCommand($db,['count'=>$table,'query'=>$query])->toArray();
+		$row = reset($res);
+		if($row->ok) {
+			return $row->n;
+		} else {
+			print_r($row);
+			return null;
+		}
+	}
+	public function collectionDrop($db,$collection) {
+		$res = $this->dbCommand($db,["drop" => $collection]);
+	}
+	public function collectionCreate($db,$collection) {
+		$res = $this->dbCommand($db,["create" => $collection]);
+	}
+	public function dbDrop($db) {
+		$res = $this->dbCommand($db,["drop" => 1]);
 	}
 }
 ?>
