63eb49ea484c4d7dfea579200556ae97592b8ef2
Michael LeSane <michael@lasc.home>
2018-06-12 20:09:14 -0500

    rename testMongoCreate -> testMongCreateClient, fix typo

--- tests/traits/mongo.php
+++ tests/traits/mongo.php
@@ -4,7 +4,7 @@ trait mongoTests {
 		$this->assertTrue(class_exists("MongoDB\Driver\Manager"));
 		emit("Mongo driver class found.",true);
 	}
-	public function testMongoCreate() {
+	public function testMongoCreateClient() {
 		$this->testMongoDriver();
 		$this->assertTrue(class_exists("dbClient"));
 		emit("Creating MongoDB client...",true);
@@ -14,7 +14,7 @@ trait mongoTests {
 	}
 	public function testMongoDbList($db = null) {
 		if(empty($db)) {
-			$db = $this->testMongoCreate();
+			$db = $this->testMongoCreateClient();
 		}
 		$list = $db->showDatabases();
 		$this->assertTrue(is_array($list));
@@ -22,19 +22,19 @@ trait mongoTests {
 		return $list;
 	}
 	public function testMongoCreateCollection($dbName = "cogTest", $collection = "testCollection",$attempt2 = false) {
-		$db = $this->testMongoCreate();
+		$db = $this->testMongoCreateClient();
 		try {
 			$db->collectionCreate($dbName,$collection);
 		} catch (Exception $e) {
 			$this->assertFalse($attempt2,print_r($e,1));
 			$db->collectionDrop($dbName,$collection);
-			$this->testMongCreateCollection($dbName,$collection,true);
+			$this->testMongoCreateCollection($dbName,$collection,true);
 		}
 		$collections = $db->showCollections($dbName);
 		$this->assertTrue(in_array($collection,$collections));
 	}
 	public function testMongoDropCollection($dbName = "cogTest",$collection = "testCollection") {
-		$db = $this->testMongoCreate();
+		$db = $this->testMongoCreateClient();
 		$db->collectionDrop($dbName,$collection);
 		$collections = $db->showCollections($dbName);
 		$this->assertFalse(in_array($collection,$collections));
