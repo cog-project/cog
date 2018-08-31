e2f0afd3bf5bb9f472d2a9085d609047a8ba6863
Michael LeSane <michael@lasc.home>
2018-08-31 17:26:50 -0500

    add helper functions to cog class

--- cog.class.php
+++ cog.class.php
@@ -1,8 +1,33 @@
 <?php
 class cog {
-	static function generate_nonce($counter,&$hash,$prefix = 'cog') {
-		$hash = hash("sha256",$counter);
+	static $version = 0;
+
+	static function hash($x) {
+		return hash("sha256",$x);
+	}
+	
+	static function generate_nonce($counter) {
 		# todo - verify this before network storage - and make sure the original unhashed string is visible.
+		return self::hash($counter);
+	}
+
+	static function generate_zero_hash() {
+		$nonce = self::generate_nonce("");
+		for($i = 0; $i < strlen($nonce); $i++) {
+			$nonce[$i] = '0';
+		}
+		return $nonce;
+	}
+
+	static function generate_header($prevHash = null,$counter = 0,$address = null) {
+		$header = [
+			'version' => self::$version,
+			'prevHash' => $prevHash,
+			'timestamp' => gmdate('Y-m-d H:i:s\Z'),
+			'counter' => $counter,
+			'address' => $address,
+		];
+		return json_encode($header,JSON_PRETTY_PRINT);
 	}
 }
 ?>
