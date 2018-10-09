9b0c77823fd25547af89a9c5e046c4371e300af3
Michael LeSane <michael@lasc.home>
2018-10-09 18:17:18 -0500

    buildout cog class - generate_keypair, encrypt, decrypt, sign

--- cog.class.php
+++ cog.class.php
@@ -29,5 +29,55 @@ class cog {
 		];
 		return json_encode($header,JSON_PRETTY_PRINT);
 	}
+
+	static function generate_keypair() {
+		$keypair = [
+			'pub' => null,
+			'priv' => null,
+		];
+		
+		// Configuration settings for the key
+		$config = array(
+		    "digest_alg" => "sha512",
+		    "private_key_bits" => 4096,
+		    "private_key_type" => OPENSSL_KEYTYPE_RSA,
+		);
+
+		// Create the private and public key
+		$res = openssl_pkey_new($config);
+
+		// Extract the private key into $private_key
+		openssl_pkey_export($res, $keypair['priv']);
+
+		// Extract the public key into $public_key
+		$pub = openssl_pkey_get_details($res);
+		$keypair['pub'] = $pub["key"];
+
+		return $keypair;
+	}
+
+	static function encrypt($pub,$data) {
+		if (openssl_public_encrypt($data, $encrypted, $pub)) {
+			$data = base64_encode($encrypted);
+		} else {
+			throw new Exception('Unable to encrypt data. Perhaps it is bigger than the key size?');
+		}
+		return $data;
+	}
+
+	static function decrypt($priv,$data) {
+		if (openssl_private_decrypt(base64_decode($data), $decrypted, $priv)) {
+			$data = $decrypted;
+			return $data;
+		} else {
+			throw new Exception('Failed to decrypt data.');
+		}
+	}
+
+	static function sign($priv,$data) {
+		openssl_sign($data, $binary_signature, $priv, OPENSSL_ALGO_SHA1);
+		$sig = base64_encode($binary_signature);
+		return $sig;
+	}
 }
 ?>
