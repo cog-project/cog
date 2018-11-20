8c2efdf60634b9eb9df79befc3969cf230c5395c
Michael LeSane <michael@lasc.home>
2018-11-20 00:20:43 -0600

    home element - add environment details, update panel element render arguments

--- elements/home.php
+++ elements/home.php
@@ -2,11 +2,17 @@
 <div class='main'>
   <h2>Address</h2>
   <b><?=$client->getAddress()?></b>
+
+  <h2>Network</h2>
+  <?php
+  $env = $client->getEnvironment();
+  echo "<b>$env</b>".($env != 'cog' ? " <span class='gray'>(Testnet)</span>":" <span class='gray'>(Live)</span>");
+  ?>
   
   <h2>Options</h2>
   <?php if(!$isRegistered) {
-    renderElement('panel_invite');
+    renderElement('panel_invite',['client'=>$client]);
   } else {
-    renderElement('panel_main');
+    renderElement('panel_main',['summary' => $summary]);
   } ?>
 </div>
