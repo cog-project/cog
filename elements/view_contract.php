88f82f723cd529c8389739c6229f6005bbd02b50
Michael LeSane <michael@lasc.home>
2019-02-05 17:44:38 -0600

    add view_contract view

---
+++ elements/view_contract.php
@@ -0,0 +1,57 @@
+<h2>Contract Information</h2>
+<h3>Headers</h3>
+<table class='view'>
+ <tbody>
+  <tr>
+   <td>Hash</td>
+   <td><?=$data['hash']?></td>
+  </tr>
+  <tr>
+   <td>Version</td>
+   <td><?=$data['request']['headers']['version']?></td>
+  </tr>
+  <tr>
+   <td>PrevHash</td>
+   <td>
+     <?=($data['request']['headers']['prevHash'] == cog::generate_zero_hash()) ? "{$data['request']['headers']['prevHash']} <span class='gray'>(N/A)</span>" : "<a href='?view_contract={$data['request']['headers']['prevHash']}'>{$data['request']['headers']['prevHash']}</a>"?>
+   </td>
+  </tr>
+  <tr>
+   <td>TimeStamp</td>
+   <td><?=$data['request']['headers']['timestamp']?></td>
+  </tr>
+  <tr>
+   <td>Nonce</td>
+   <td><?=$data['request']['headers']['counter']?></td>
+  </tr>
+  <tr>
+   <td>Created By</td>
+   <td><?=$data['request']['headers']['address'] . ($data['request']['headers']['address'] == $client->getAddress() ? " <span class='gray'>(You)</span>" : "")?></td>
+  </tr>
+ </tbody>
+</table>
+<h3>Data</h3>
+<table class='view'>
+ <tbody>
+  <tr>
+   <td>Action</td>
+   <td><b><?=$data['request']['action']?></b></td>
+  </tr>
+  <tr>
+   <td>Database</td>
+   <td><?=$data['request']['params']['database']?></td>
+  </tr>
+  <tr>
+   <td>Address</td>
+   <td><?=$data['request']['params']['address'] . ($data['request']['headers']['address'] == $client->getAddress() ? " <span class='gray'>(You)</span>" : "")?></td>
+  </tr>
+  <tr>
+   <td>Public Key</td>
+   <td class='txt'><?=$data['request']['params']['public_key']?></td>
+  </tr>
+  <tr>
+   <td>Signature</td>
+   <td class='txt'><?=$data['request']['signature']?></td>
+  </tr>
+ </tbody>
+</table>
