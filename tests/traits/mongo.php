619dd5bf3c6cb24f3891ce25ef1d02e93eb1a932
root <root@lasc.home>
2019-04-14 19:13:10 -0500

    cogtest cleanup

--- tests/traits/mongo.php
+++ tests/traits/mongo.php
@@ -1,7 +1,63 @@
 <?php
 trait mongoTests {
+	public function testMultiDelete() {
+		$db = new dbClient();
+
+		// Insert
+
+		$data = [];
+		for($i = 0; $i < 3; $i++) {
+			$data[] = ['why' => rand()];
+		}
+
+		$db->dbInsertMultiple('cogTest.blocks',$data);
+
+		// Verify
+
+		$rows = $db->dbQuery('cogTest.blocks',['why'=>['$ne'=>null]]);
+		$this->assertTrue(count($rows) == count($data));
+		foreach($rows as $row) {
+			$match = false;
+			foreach($data as $orig) {
+				if($orig['why'] == $row['why']) {
+					$match = true;
+					break;
+				}
+			}
+			$this->assertTrue($match);
+		}
+
+		// Update
+		$criteria = [];
+		foreach($rows as &$row) {
+			$row['because'] = rand();
+			$db->dbUpdate("cogTest.blocks",$row,['why'=>$row['why']]);
+		}
+
+		// Verify
+		$updated_rows = $db->dbQuery('cogTest.blocks',['why'=>['$ne'=>null],'because'=>['$ne'=>null]]);
+		$this->assertTrue(count($rows) == count($updated_rows));
+		foreach($updated_rows as $row) {
+			$match = false;
+			foreach($rows as $orig) {
+				if($orig['why'] == $row['why'] && $orig['because'] == $row['because']) {
+					$match = true;
+					break;
+				}
+			}
+			$this->assertTrue($match);
+		}
+
+		// Delete
+		$db->dbDeleteMultiple("cogTest.blocks",$updated_rows);
+
+		// Verify
+		$updated_rows = $db->dbQuery('cogTest.blocks',['why'=>['$ne'=>null],'because'=>['$ne'=>null]]);
+		$this->assertTrue(!count($updated_rows));
+	}
+
 	public function testMongoDriver() {
-		$this->assertTrue(class_exists("MongoDB\Driver\Manager"));
+		$this->assertTrue(class_exists("MongoInterface"));
 		emit("Mongo driver class found.",true);
 	}
 	public function testMongoCreateClient() {
@@ -35,7 +91,7 @@ trait mongoTests {
 	}
 	public function testMongoDropCollection($dbName = "cogTest",$collection = "blocks") {
 		$list = array_map(function($x){
-			return $x->name;
+			return $x['name'];
 		}, $this->testMongoDbList());
 		if(!in_array($dbName,$list)) {
 			$this->testMongoCreateCollection($dbName,$collection);
