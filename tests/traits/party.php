61c05059f8d86efe35d761ed8404871f10354dc6
root <root@lasc.home>
2019-04-22 14:03:00 -0500

    testParty update

--- tests/traits/party.php
+++ tests/traits/party.php
@@ -1,10 +1,12 @@
 <?php
 trait partyTests {
-	public function testParty() {
+	public function testParty($p = null,$str = "f00borx!!") {
 		# smoke testing keypairs so we don't suffer debugging if these ever fail
 		$this->assertTrue(class_exists("party"));
-		$p = new party();
-		$this->assertTrue($p->decrypt($p->encrypt("f00borx!!")) == "f00borx!!");
+		if(!is_object($p)) {
+			$p = new party();
+		}
+		$this->assertTrue($p->decrypt($p->encrypt($str)) == $str);
 		return $p;
 	}
 }
