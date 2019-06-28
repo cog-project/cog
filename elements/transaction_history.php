14fec6602c169213f6d6508c82889c5b64b8ddab
root <root@thinkdebian.home>
2019-06-28 16:49:53 -0500

    add transaction_history elem

---
+++ elements/transaction_history.php
@@ -0,0 +1,25 @@
+<h2>Mutual Transaction History</h2>
+<h3>User Details</h3>
+<table class='view'>
+  <tbody>
+    <tr>
+      <th>Nickname</th>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th>Address</th>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th>Public Key</th>
+      <td>-</td>
+    </tr>
+  </tbody>
+</table>
+<h3>Transactions</h3>
+<table class='view'>
+  <thead>
+  </thead>
+  <tbody>
+  </tbody>
+</table>
