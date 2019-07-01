de9cf46f94cea00dc7571a34f33ed19798f9cea8
root <root@lasc.home>
2019-07-01 11:17:51 -0500

    present state - mutual transaction history view

--- elements/transaction_history.php
+++ elements/transaction_history.php
@@ -1,24 +1,41 @@
+<?php
+cog::emit($creditInfo);
+?>
 <h2>Mutual Transaction History</h2>
 <h3>User Details</h3>
+
 <table class='view'>
   <tbody>
+<!--
     <tr>
       <th>Nickname</th>
       <td>-</td>
     </tr>
+-->
     <tr>
       <th>Address</th>
       <td>-</td>
     </tr>
+<!--
     <tr>
       <th>Public Key</th>
       <td>-</td>
     </tr>
+-->
   </tbody>
 </table>
 <h3>Transactions</h3>
 <table class='view'>
   <thead>
+    <tr>
+      <th>Hash</th>
+      <th>Sender</th>
+      <th>Recipient</th>
+      <th>Amount</th>
+      <th>Message</th>
+      <th>Balance</th>
+      <th>Timestamp</th>
+    </tr>
   </thead>
   <tbody>
   </tbody>
