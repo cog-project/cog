1a9e36942cd474f570513017dcec3fd430543242
root <root@thinkdebian.home>
2019-07-02 13:16:25 -0500

    transaction history view updates

--- elements/transaction_history.php
+++ elements/transaction_history.php
@@ -1,10 +1,19 @@
 <?php
-cog::emit($creditInfo);
+# TODO this REALLY should be in mongo...
+uasort($creditInfo,function($a,$b) {
+  if (strtotime($a['request']['headers']['timestamp']) == strtotime($b['request']['headers']['timestamp'])) {
+    return 0;
+  } elseif (strtotime($a['request']['headers']['timestamp']) > strtotime($b['request']['headers']['timestamp'])) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
 ?>
 <h2>Mutual Transaction History</h2>
 <h3>User Details</h3>
 
-<table class='view'>
+<table class='view small'>
   <tbody>
 <!--
     <tr>
@@ -14,7 +23,7 @@ cog::emit($creditInfo);
 -->
     <tr>
       <th>Address</th>
-      <td>-</td>
+      <td><?=$_GET['transaction_history']?></td>
     </tr>
 <!--
     <tr>
@@ -25,12 +34,10 @@ cog::emit($creditInfo);
   </tbody>
 </table>
 <h3>Transactions</h3>
-<table class='view'>
+<table class='view small'>
   <thead>
     <tr>
       <th>Hash</th>
-      <th>Sender</th>
-      <th>Recipient</th>
       <th>Amount</th>
       <th>Message</th>
       <th>Balance</th>
@@ -38,5 +45,29 @@ cog::emit($creditInfo);
     </tr>
   </thead>
   <tbody>
+  <?php
+  $render = '';
+  $balance = 0;
+  foreach($creditInfo as $t) {
+    foreach($t['request']['params']['inputs'] as $i) {
+      $amt = ($i['from'] == $client->getAddress() ? '-1' : '1') * (float)$i['amount'];
+      $balance += $amt;
+      $render = "
+    <tr>
+      <td>
+        <a href='?view_contract={$t['hash']}'>
+          {$t['hash']}
+	</a>
+      </td>
+      <td>{$amt}</td>
+      <td>{$i['message']}</td>
+      <td>{$balance}</td>
+      <td>{$t['request']['headers']['timestamp']}</td>
+    </tr>
+  " . $render;
+    }
+  }
+  echo $render;
+  ?>
   </tbody>
 </table>
